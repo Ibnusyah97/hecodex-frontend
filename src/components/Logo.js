d2bd00bef010eb246639001cdbd71b09b9fe2722
HDX007 <hecodextools@gmail.com>
2022-08-16 18:34:21 +0700

    update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import Hecodex from '../assets/images/logo.png';
+import HecodexLight from '../assets/images/logo-light.png';
 
 const LogoStyles = styled.div`
-  max-width: 100px;
-  svg {
+  max-width: 125px;
+  img {
     width: 100%;
     height: 100%;
     object-fit: contain;
+    margin-top: 0.5rem;
+  }
+  @media only screen and (max-width: 768px) {
+    max-width: 110px;
   }
 `;
 
 export default function Logo({ ...rest }) {
   return (
     <LogoStyles {...rest}>
-      <img src={Hecodex} alt="Hecodex" />
+      <img src={HecodexLight} alt="Hecodex" />
     </LogoStyles>
   );
 }
--- src/components/buttons/SwapButton.js
+++ src/components/buttons/SwapButton.js
@@ -26,7 +26,7 @@ const ButtonsStyles = styled.a`
   margin-left: 1rem;
   @media only screen and (max-width: 768px) {
     font-size: 1.2rem;
-    padding: 0.6rem 2.1rem;
+    padding: 0.7rem 2.1rem;
     margin-right: 1.2rem;
   }
 `;
